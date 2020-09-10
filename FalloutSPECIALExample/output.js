7a06bfbdb2cd332869f3860b54d6f9c4bca0515a
Christian <50080567+ChrisNonyminus@users.noreply.github.com>
2020-09-10 08:46:41 -0600

    Update output.js

--- FalloutSPECIALExample/output.js
+++ FalloutSPECIALExample/output.js
@@ -1,4 +1,3 @@
-
 // Checkout the repo examples to get an idea of other ways you can use scripting 
 // https://github.com/AIDungeon/Scripting/blob/master/examples
 const modifier = (text) => {
@@ -10,7 +9,7 @@ const modifier = (text) => {
   {
     if ((text.includes('You are Level '+(i+1))) || (text.includes('You are now Level '+(i+1))))
     {
-     state.memory = state.memory + '\nYour skills:\nBarter: '+(2+(2*(state.playerCharisma+1))+((state.playerLuck+1)/2)+state.barterBonus)+'\nEnergy Weapons: '+(2+(2*(state.playerPerception+1))+((state.playerLuck+1)/2)+state.energyWepBonus)+'\nExplosives: '+(2+(2*(state.playerPerception+1))+((state.playerLuck+1)/2)+state.explosivesBonus)+'\nGuns: '+(2+(2*(state.playerAgility+1))+((state.playerLuck+1)/2)+state.gunsBonus)+'\nLockpick: '+(2+(2*(state.playerPerception+1))+((state.playerLuck+1)/2)+state.lockpickBonus)+'\nMedicine: '+(2+(2*(state.playerIntelligence+1))+((state.playerLuck+1)/2)+state.medicineBonus)+'\nMelee Weapons: '+(2+(2*(state.playerStrength+1))+((state.playerLuck+1)/2)+state.meleeWepBonus)+'\nRepair: '+(2+(2*(state.playerIntelligence+1))+((state.playerLuck+1)/2)+state.repairBonus)+'\nScience: '+(2+(2*(state.playerIntelligence+1))+((state.playerLuck+1)/2)+state.scienceBonus)+'\nSneak: '+(2+(2*(state.playerAgility+1))+((state.playerLuck+1)/2)+state.sneakBonus)+'\nSpeech: '+(2+(2*(state.playerCharisma+1))+((state.playerLuck+1)/2)+state.speechBonus)+'\nSurvival: '+(2+(2*(state.playerEndurance+1))+((state.playerLuck+1)/2)+state.survivalBonus)+'\nUnarmed: '+(2+(2*(state.playerEndurance+1))+((state.playerLuck+1)/2)+state.unarmedBonus)
+  state.memory.context = state.memory.context + '\nYour skills:\nBarter: '+state.Barter+'\nEnergy Weapons: '+state.EnergyWeapons+'\nExplosives: '+state.Explosives+'\nGuns: '+Guns+'\nLockpick: '+Lockpick+'\nMedicine: '+Medicine+'\nMelee Weapons: '+MeleeWeapons+'\nRepair: '+Repair+'\nScience: '+Science+'\nSneak: '+Sneak+'\nSpeech: '+Speech+'\nSurvival: '+Survival+'\nUnarmed: '+Unarmed
 
      state.playerLevel = i+1
     }
@@ -22,19 +21,19 @@ const modifier = (text) => {
   if(state.playerXP>=100)
   {
     modifiedText = text + '\nLEVEL UP\nYou are now Level '+(state.playerLevel+1)+'!' +'\nYou gain a random perk: ' //random because I couldn't be bothered to define every perk and have a script to let players choose, so we'll just hope the AI doesn't fuck up
-    state.barterBonus += state.skillPoints/2 //Likewise, I couldn't be bothered to destribute skill points between skills upon level up
-    state.energyWepBonus += state.skillPoints/2
-    state.explosivesBonus += state.skillPoints/2
-    state.gunsBonus += state.skillPoints/2
-    state.lockpickBonus += state.skillPoints/2
-    state.medicineBonus += state.skillPoints/2
-    state.meleeWepBonus += state.skillPoints/2
-    state.repairBonus += state.skillPoints/2
-    state.scienceBonus += state.skillPoints/2
-    state.sneakBonus += state.skillPoints/2
-    state.speechBonus += state.skillPoints/2
-    state.survivalBonus += state.skillPoints/2
-    state.unarmedBonus += state.skillPoints/2
+    state.Barter += state.skillPoints/2 //Likewise, I couldn't be bothered to destribute skill points between skills upon level up
+    state.EnergyWeapons += state.skillPoints/2
+    state.Explosives += state.skillPoints/2
+    state.Guns += state.skillPoints/2
+    state.Lockpick += state.skillPoints/2
+    state.Medicine += state.skillPoints/2
+    state.MeleeWeapons += state.skillPoints/2
+    state.Repair += state.skillPoints/2
+    state.Science += state.skillPoints/2
+    state.Sneak += state.skillPoints/2
+    state.Speech += state.skillPoints/2
+    state.Survival += state.skillPoints/2
+    state.Unarmed += state.skillPoints/2
     state.playerXP = 0
   }
  state.message = JSON.stringify(state.memory);
